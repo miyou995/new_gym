325e0ff2db858aa2d4b52ada6628ac41fb12d988
unknown <inter.taki@gmail.com>
2022-07-26 14:14:11 +0100

    ubvenv deleted

--- ubvenv/lib/python3.8/site-packages/test_project/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestProjectConfig(AppConfig):
-    name = "test_project"
--- ubvenv/lib/python3.8/site-packages/test_project/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from test_project.views import ChildBViewSet
-
-router = DefaultRouter()
-router.register("childb", ChildBViewSet, basename="childb")
-
-urlpatterns = router.urls
