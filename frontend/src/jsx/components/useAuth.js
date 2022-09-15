8ac609b9a90566db52cd43bd2f6651bf323b2084
ElhachemiAm <ameur.elhachemi@gmail.com>
2022-09-15 12:53:05 +0000

    b6

--- frontend/src/jsx/components/useAuth.js
+++ frontend/src/jsx/components/useAuth.js
@@ -2,20 +2,25 @@ import { useEffect, useState } from "react";
 import useAxios from "./useAxios";
 
 
- const useAuth = (url, method) => {
+ const useAuth = (url, method, payload) => {
     const [auth, setauth] = useState(null)
+    const [data, setData] = useState(null)
+
     const api = useAxios();
     useEffect(  () => {
-       api.request({method,url}).then( res => {
+       api.request({ data: payload, method,url}).then( res => {
        if (res.status == 200) {
+        setData(res.data)
         setauth(true) 
        } else {
-          setauth(false) 
+         setauth(false) 
        }
      }).catch( err => {
-        setauth(false) 
+         console.log(err.response.status)
+         setauth(false) 
      })
    }, [url])
+
    return auth
 };
 export default useAuth;
