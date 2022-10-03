9bf9e967e0ed9ba690f23271c29bae865b4ce7c6
miyou995 <inter.taki@gmail.com>
2022-10-03 12:13:16 +0100

    new ceeeeeeleeeryyy

--- backend/client/apps.py
+++ backend/client/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class ClientConfig(AppConfig):
     name = 'client'
+    
+    def read(self):
+        print('im DONE APP CLIENT---------------------------------------------------------------------------------')
