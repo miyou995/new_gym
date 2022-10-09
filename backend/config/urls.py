016270a8a66a4dda310e7891285e72ceb093c79b
miyou995 <inter.taki@gmail.com>
2022-10-09 11:03:31 +0100

    new com

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -46,9 +46,17 @@ urlpatterns = [
     # path('markdownx/', include('markdownx.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),] 
+# else:
+#     path ('',TemplateView.as_view(template_name="index.html"), name='index'),
+#     re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html"), name='index'),
+
+
+
+    
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
