74b2e0c36343034c3d6dae1d9d5fc043521bc0e6
taki eddine <inter.taki@gmail.com>
2023-03-19 10:20:55 +0000

    looord

--- backend/salle_activite/models.py
+++ backend/salle_activite/models.py
@@ -8,17 +8,11 @@ class SalleManager(models.Manager):
             salle = Salle.objects.filter(is_default=True).last()
         return salle
 
-class Door(models.Model):
-    ip_adress = models.CharField(max_length=20, verbose_name="Adresse ip de la porte")
-    username  = models.CharField(max_length=50, verbose_name="username de la porte")
-    password  = models.CharField(max_length=30, verbose_name="password de la porte")
-    def __str__(self):
-        return self.ip_adress
+
 
 
 class Salle(models.Model):
     name = models.CharField(max_length=50, verbose_name="nom de la salle d'activité")
-    door = models.ForeignKey(Door, on_delete=models.CASCADE, related_name='salles', blank=True, null=True)
     is_default      = models.BooleanField(default=False)
     # coach = models.ManyToManyField("app.Coach")
     objects = models.Manager()
@@ -38,7 +32,14 @@ class Salle(models.Model):
             self.is_default = True
             update_default = Salle.objects.all().exclude(id=self.id).update(is_default=False)
         super(Salle, self).save(*args, **kwargs)
-
+        
+class Door(models.Model):
+    ip_adress = models.CharField(max_length=20, verbose_name="Adresse ip de la porte")
+    door = models.ForeignKey(Salle, on_delete=models.CASCADE, related_name='doors', blank=True, null=True)
+    username  = models.CharField(max_length=50, verbose_name="username de la porte")
+    password  = models.CharField(max_length=30, verbose_name="password de la porte")
+    def __str__(self):
+        return self.ip_adress
 
 class Activity(models.Model):
     name    = models.CharField( max_length=150, verbose_name="nom d'activité")
