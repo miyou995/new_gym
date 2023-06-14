dfce9eef43aa83c09bc70a69c95d3f0bbd45a2bb
miyou995 <inter.taki@gmail.com>
2023-06-14 08:18:44 +0100

    new treatment

--- backend/materiel/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MaterielConfig(AppConfig):
-    name = 'materiel'
--- backend/materiel/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Materiel
-from rest_framework import serializers
-
-
-class MaterielSerialiser(serializers.ModelSerializer):
-    class Meta:
-        model = Materiel
-        fields= '__all__'
