06aad062be610f9e4e8953a69f5038060f955a98
taki eddine <inter.taki@gmail.com>
2023-11-06 15:03:54 +0100

    voicing

--- backend/config/local_settings.py
+++ backend/config/local_settings.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 SECRET_KEY = 'QCqcqscrtgjczvgrezg0hzd6t%82b3ol#^)6(94^o+nto(5h#kg#f7z!yh8'
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*'] # a modifier apres l'integration du nom de domaine
 
@@ -18,12 +18,14 @@ DATABASES = {
     }
 }
 
+
+
 STOP_HOUR   = 4
 STOP_MINUTE = 1
 
-
 START_HOUR   = 4
 START_MINUTE = 11
+
 
 
 if DEBUG == True:
