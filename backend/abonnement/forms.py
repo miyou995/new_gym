0048bd2dcdaad2b26208df55a89136c1856d1e64
riadh hadjalla <yousra09blida1995@gmail.com>
2024-09-23 17:13:27 +0100

    calender23

--- backend/abonnement/forms.py
+++ backend/abonnement/forms.py
@@ -54,3 +54,45 @@ class AbonnemetsClientModelForm(forms.ModelForm):
             'required': 'veuillez choisir.',
             'invalid': 'Custom error message for field2 is invalid.',
         }
+
+
+from datetime import  timedelta
+
+from django import forms
+from .models import AbonnementClient
+from creneau.models import Creneau
+
+class UpdateAbonnementClientForm(forms.ModelForm):
+    # Define any additional fields that are not part of the model, if necessary
+    event_pk = forms.ModelMultipleChoiceField(
+        queryset=Creneau.objects.all(),
+        widget=forms.CheckboxSelectMultiple,  # You can adjust the widget as per your needs
+        required=True,
+        label="Select Events"
+    )
+    today = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True)
+
+    class Meta:
+        model = AbonnementClient
+        fields = ['type_abonnement', 'start_date', 'end_date']
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateAbonnementClientForm, self).__init__(*args, **kwargs)
+        # If needed, set an initial value for `today`
+        self.fields['today'].initial = kwargs.get('initial', {}).get('today', None)
+
+    def save(self, commit=True):
+        abonnement_client = super().save(commit=False)  # Get the AbonnementClient instance
+        
+        # Get the event primary keys and fetch the Creneaux instances
+        selected_creneaux = self.cleaned_data['event_pk']
+        
+        # Update abonnement_client fields
+        abonnement_client.start_date = self.cleaned_data['today']
+        abonnement_client.end_date = abonnement_client.start_date + timedelta(days=30)
+
+        if commit:
+            abonnement_client.save()  # Save the AbonnementClient instance first
+            abonnement_client.creneaux.set(selected_creneaux)  # Update many-to-many field
+
+        return abonnement_client
