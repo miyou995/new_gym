02c627bce24da7769de234c383e40e737b585efe
riadh hadjalla <yousra09blida1995@gmail.com>
2024-09-26 15:56:06 +0100

    blockingdate

--- backend/abonnement/forms.py
+++ backend/abonnement/forms.py
@@ -2,97 +2,109 @@ from django import forms
 from django.contrib.auth import authenticate
 import logging
 from .models import AbonnementClient
-from client.models import Client
 
 
 logger = logging.getLogger(__name__)
 
 
-class AbonnemetsClientModelForm(forms.ModelForm):
-    client = forms.ModelChoiceField(queryset=Client.objects.all())
-
-    class Meta:
-          model=AbonnementClient
-          fields =(
-                'start_date',
-                'end_date',
-               'client',
-               'creneaux',
-               'type_abonnement',
-               
-          )
-    def __init__(self, ticket=None, *args, **kwargs):   
-        super().__init__(*args, **kwargs)
-
-        initial= kwargs.get('initial',{})
-        client_pk=initial.get('client_pk')
-        if client_pk :
-            try :
-                initial['client']=Client.objects.get(pk=client_pk)
-                print("work ftom tryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
-                # initial['abonnement_client']=AbonnementClient.objects.filter(client=client_pk)
-            except Client.DoesNotExist:
-                pass
-        kwargs['initial']=initial
-        super(AbonnemetsClientModelForm,self).__init__(*args,**kwargs)
-
-
-        self.fields['client'].error_messages = {
-            'required': 'veuillez choisir.',
-            'invalid': 'Custom error message for field1 is invalid.',
-          
-        }
-        self.fields['creneaux'].error_messages = {
-            'required': 'veuillez choisir.',
-            'invalid': 'Custom error message for field2 is invalid.',
-        }
-        self.fields['type_abonnement'].error_messages = {
-            'required': 'veuillez choisir.',
-            'invalid': 'Custom error message for field2 is invalid.',
-        }
-        self.fields['start_date'].error_messages = {
-            'required': 'veuillez choisir.',
-            'invalid': 'Custom error message for field2 is invalid.',
-        }
-
-
-from datetime import  timedelta
-
-from django import forms
-from .models import AbonnementClient
-from creneau.models import Creneau
-
-class UpdateAbonnementClientForm(forms.ModelForm):
-    # Define any additional fields that are not part of the model, if necessary
-    event_pk = forms.ModelMultipleChoiceField(
-        queryset=Creneau.objects.all(),
-        widget=forms.CheckboxSelectMultiple,  # You can adjust the widget as per your needs
-        required=True,
-        label="Select Events"
-    )
-    today = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True)
-
+class AbonnementClientRestTempForm(forms.ModelForm):
     class Meta:
         model = AbonnementClient
-        fields = ['type_abonnement', 'start_date', 'end_date']
+        fields = ( "presence_quantity",)
 
-    def __init__(self, *args, **kwargs):
-        super(UpdateAbonnementClientForm, self).__init__(*args, **kwargs)
-        # If needed, set an initial value for `today`
-        self.fields['today'].initial = kwargs.get('initial', {}).get('today', None)
+class AbonnementClientRestPaiementForm(forms.ModelForm):
+    class Meta:
+        model = AbonnementClient
+        fields = ( "reste",)
 
-    def save(self, commit=True):
-        abonnement_client = super().save(commit=False)  # Get the AbonnementClient instance
+# class AbonnemetsClientModelForm(forms.ModelForm):
+#     client = forms.ModelChoiceField(queryset=Client.objects.all())
+
+#     class Meta:
+#           model=AbonnementClient
+#           fields =(
+#                 'start_date',
+#                 'end_date',
+#                'client',
+#                'creneaux',
+#                'type_abonnement',
+               
+#           )
+#     def __init__(self, ticket=None, *args, **kwargs):   
+#         super().__init__(*args, **kwargs)
+
+#         initial= kwargs.get('initial',{})
+#         client_pk=initial.get('client_pk')
+#         if client_pk :
+#             try :
+#                 initial['client']=Client.objects.get(pk=client_pk)
+#                 print("work ftom tryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+#                 # initial['abonnement_client']=AbonnementClient.objects.filter(client=client_pk)
+#             except Client.DoesNotExist:
+#                 pass
+#         kwargs['initial']=initial
+#         super(AbonnemetsClientModelForm,self).__init__(*args,**kwargs)
+
+
+#         self.fields['client'].error_messages = {
+#             'required': 'veuillez choisir.',
+#             'invalid': 'Custom error message for field1 is invalid.',
+          
+#         }
+#         self.fields['creneaux'].error_messages = {
+#             'required': 'veuillez choisir.',
+#             'invalid': 'Custom error message for field2 is invalid.',
+#         }
+#         self.fields['type_abonnement'].error_messages = {
+#             'required': 'veuillez choisir.',
+#             'invalid': 'Custom error message for field2 is invalid.',
+#         }
+#         self.fields['start_date'].error_messages = {
+#             'required': 'veuillez choisir.',
+#             'invalid': 'Custom error message for field2 is invalid.',
+#         }
+
+
+# from datetime import  timedelta
+
+# from django import forms
+# from .models import AbonnementClient
+# from creneau.models import Creneau
+
+# class UpdateAbonnementClientForm(forms.ModelForm):
+#     # Define any additional fields that are not part of the model, if necessary
+#     event_pk = forms.ModelMultipleChoiceField(
+#         queryset=Creneau.objects.all(),
+#         widget=forms.CheckboxSelectMultiple,  # You can adjust the widget as per your needs
+#         required=True,
+#         label="Select Events"
+#     )
+#     today = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True)
+
+#     class Meta:
+#         model = AbonnementClient
+#         fields = ['type_abonnement', 'start_date', 'end_date']
+
+#     def __init__(self, *args, **kwargs):
+#         super(UpdateAbonnementClientForm, self).__init__(*args, **kwargs)
+#         # If needed, set an initial value for `today`
+#         self.fields['today'].initial = kwargs.get('initial', {}).get('today', None)
+
+#     def save(self, commit=True):
+#         abonnement_client = super().save(commit=False)  # Get the AbonnementClient instance
         
-        # Get the event primary keys and fetch the Creneaux instances
-        selected_creneaux = self.cleaned_data['event_pk']
+#         # Get the event primary keys and fetch the Creneaux instances
+#         selected_creneaux = self.cleaned_data['event_pk']
         
-        # Update abonnement_client fields
-        abonnement_client.start_date = self.cleaned_data['today']
-        abonnement_client.end_date = abonnement_client.start_date + timedelta(days=30)
+#         # Update abonnement_client fields
+#         abonnement_client.start_date = self.cleaned_data['today']
+#         abonnement_client.end_date = abonnement_client.start_date + timedelta(days=30)
 
-        if commit:
-            abonnement_client.save()  # Save the AbonnementClient instance first
-            abonnement_client.creneaux.set(selected_creneaux)  # Update many-to-many field
+#         if commit:
+#             abonnement_client.save()  # Save the AbonnementClient instance first
+#             abonnement_client.creneaux.set(selected_creneaux)  # Update many-to-many field
 
-        return abonnement_client
+#         return abonnement_client
+
+
+
--- backend/abonnement/urls.py
+++ backend/abonnement/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
-                    abc_htmx_view,CalendarAbonnementClient,add_abonnement_client,update_abonnement_client
+                    abc_htmx_view,CalendarAbonnementClient,add_abonnement_client,update_abonnement_client,
+                    update_temps_rest,update_paiement_rest,renew_abonnemetn_client
 )
 # , RenewalSubscription
 from .views import *
@@ -14,11 +15,12 @@ urlpatterns = [
     path('add_abonnement_client/<str:client_pk>/<str:type_abonnement>', add_abonnement_client, name='add_abonnement_client'),
 
 
-
-
     path('CalendarUpdateAbonnementClient/<str:pk>', CalendarUpdateAbonnementClient.as_view(), name='calendar_update_abonnementclient'),
     path('update_abonnement_client/<int:pk>/<str:type_abonnement>', update_abonnement_client, name='update_abonnement_client'),
     path('abonnemt_client_delete/<int:pk>', AbonnemtClientDeleteView.as_view(), name='abonnemt_client_delete'),
+    path('update_temps_rest/<int:pk>/', update_temps_rest, name='update_temps_rest'),
+    path('update_paiement_rest/<int:pk>/', update_paiement_rest, name='update_paiement_rest'),
+    path('renew_abonnemetn_client/<int:pk>/', renew_abonnemetn_client, name='renew_abonnemetn_client'),
 
 
 
