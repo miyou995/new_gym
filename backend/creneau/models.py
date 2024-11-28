cf8ea687945f0b70ddd269f338610c7c1ae7a87a
taki eddine <inter.taki@gmail.com>
2024-11-28 09:22:41 +0100

    some warnigngd fixed

--- backend/creneau/models.py
+++ backend/creneau/models.py
@@ -199,11 +199,3 @@ class Event(models.Model):
 
 
 
-class Event(models.Model):
-    title = models.CharField(max_length=200)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-   
-
-    def __str__(self):
-        return self.title
