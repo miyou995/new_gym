4acf655a6ff17837bcd6355cd61f556d731a0eb6
riadh hadjalla <yousra09blida1995@gmail.com>
2024-12-11 16:34:46 +0100

    master

--- backend/config/local_settings.py
+++ backend/config/local_settings.py
@@ -59,14 +59,13 @@ CELERY_BEAT_SCHEDULE = {
 }
 
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "gym_db",
-        'USER': "postgres",
-        'PASSWORD': "postgres",
-        'HOST': "localhost",
-        'PORT': "5432",
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': "gym_db",
+#         'USER': "postgres",
+#         'PASSWORD': "postgres",
+#         'HOST': "localhost",
+#         'PORT': "5432",
+#     }
+# }
